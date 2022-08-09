514e643237714d256323510563fc3714039d08ea
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-09 17:49:10 -0400

    toggle member

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -12,6 +12,11 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  //global state and useNavigate
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
 
   const handleChange = (e) => {
     console.log(e.target);
@@ -26,15 +31,18 @@ const Register = () => {
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
         {values.showAlert && <Alert />}
         {/* name input */}
-        <FormRow
-          type="text"
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
-        />
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
         {/* email input */}
         <FormRow
           type="email"
@@ -53,6 +61,12 @@ const Register = () => {
         <button type="submit" className="btn btn-block">
           submit
         </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
