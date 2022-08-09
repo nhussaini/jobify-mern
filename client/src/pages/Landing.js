657a1bdf8e2ff0057ed2afcf60b604bd50e918bf
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-09 15:55:42 -0400

    create routes with react router dom

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,12 @@
-import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" />
+        <Logo />
       </nav>
       <div className="container page">
         {/* info */}
@@ -20,7 +21,9 @@ const Landing = () => {
             iceland, farm-to-table tumeric cold-pressed four dollar toast. Umami
             tbh seitan, banjo raclette succulents lo-fi cardigan.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
