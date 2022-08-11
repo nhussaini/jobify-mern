76a1570b1f54a72a079c4ffc4511a47b7b6cf351
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-10 21:41:10 -0400

    create a custom middleware for notfoundroute

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+//middleware
+// notFoundMiddleware;
+import notFoundMiddleware from './middleware/not-found.js';
+
 app.get('/', (req, res) => {
   res.send('welcome');
 });
+
+//app.use() is used to use a middleware
+app.use(notFoundMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
