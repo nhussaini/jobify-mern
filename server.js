ed1ddf9a39ff0b05448839b0571dc14f80f50fb3
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-10 22:03:52 -0400

    create ErrorHandlerMiddleware

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-// notFoundMiddleware;
+import errorHandlerMiddleWare from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.send('welcome');
 });
 
 //app.use() is used to use a middleware
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleWare);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
