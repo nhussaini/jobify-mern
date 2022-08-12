8be5ee59dd6b879cddb495a0be45842c6a89d47b
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-11 21:29:36 -0400

    check for empty values in controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,13 @@ import { StatusCodes } from 'http-status-codes';
 const register = async (req, res) => {
   //   console.log('user=>', req.body);
   //   res.send('register user');
+  const { name, email, password } = req.body;
 
-  const user = await User.create(req.body);
+  if (!name || !password || !email) {
+    throw new Error('please provide all values');
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleWare = (err, req, res, next) => {
-  //   console.log(err);
+  console.log(err.message);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: 'Something went Wrong, try again later',
+    msg: err.message || 'Something went Wrong, try again later',
   };
   if (err.name === 'ValidationError') {
     // console.log('err.name=>', err.name);
@@ -12,6 +12,10 @@ const errorHandlerMiddleWare = (err, req, res, next) => {
     defaultError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(',');
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
   }
   //   res.status(defaultError.statusCode).json({ msg: err });
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
