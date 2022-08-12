1a45b04ae0644805a98640f087fe8ab0950736c7
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-11 23:06:44 -0400

    Hash passwords with bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -35,6 +36,12 @@ const UserSchema = new mongoose.Schema({
     maxlength: 20,
     default: 'my city',
   },
+});
+
+//this is a mongoose middleware which will be invoked before any save()
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model('User', UserSchema);
