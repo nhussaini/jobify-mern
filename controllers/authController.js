63b4aaf8e46ad90307051e029aa09bc20475ed55
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-11 21:36:30 -0400

    create a custom apierror

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,20 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
 const register = async (req, res) => {
   //   console.log('user=>', req.body);
   //   res.send('register user');
   const { name, email, password } = req.body;
 
   if (!name || !password || !email) {
-    throw new Error('please provide all values');
+    throw new CustomAPIError('please provide all values');
   }
 
   const user = await User.create({ name, email, password });
