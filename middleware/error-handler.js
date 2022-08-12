ddd4916bc395882db3beb83fdaa1477765a794c8
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-11 21:13:17 -0400

    missng field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,20 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleWare = (err, req, res, next) => {
-  console.log(err);
+  //   console.log(err);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: 'Something went Wrong, try again later',
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === 'ValidationError') {
+    // console.log('err.name=>', err.name);
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
+  //   res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleWare;
