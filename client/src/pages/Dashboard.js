7f4e52c06cba595d9b11ab084f758ce33be76dcd
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-12 22:13:03 -0400

    Register User-setup

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,20 +1,5 @@
 import { useEffect } from 'react';
 const Dashboard = () => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch('/api/v1');
-      // console.log(response.json());
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return <h1>Dashboard</h1>;
 };
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
   //global state and useNavigate
 
   //destructre values from useContext()
-  const { isLoading, showAlert, displayAlert } = useAppContext();
+  const { isLoading, showAlert, displayAlert, registerUser } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -37,7 +37,14 @@ const Register = () => {
       displayAlert();
       return;
     }
-    console.log(values);
+    //create a new user
+    const currentUser = { name, email, password };
+    if (isMember) {
+      console.log('already a member');
+    } else {
+      //if a new user, register the user
+      registerUser(currentUser);
+    }
   };
 
   return (
@@ -71,7 +78,7 @@ const Register = () => {
           handleChange={handleChange}
         />
 
-        <button type="submit" className="btn btn-block">
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
           submit
         </button>
         <p>
