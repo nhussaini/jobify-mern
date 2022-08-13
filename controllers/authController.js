83335de13375a1f8d3ef2bb4a824c16f624d0008
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-13 14:34:38 -0400

    finish login user in backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js';
 
 // class CustomAPIError extends Error {
 //   constructor(message) {
@@ -53,6 +53,26 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials');
+  }
+
+  //if login is successful
+  const token = user.createJWT();
+  user.password = undefined; //because I don't want to send password to frontend
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
+
   res.send('login user');
 };
 
--- models/User.js
+++ models/User.js
@@ -53,4 +53,10 @@ UserSchema.methods.createJWT = function () {
   });
 };
 
+//Compare password
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
 export default mongoose.model('User', UserSchema);
