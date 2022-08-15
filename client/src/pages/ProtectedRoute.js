9086e250e8c6b5aa9662b5e02b5d2c87c12bf151
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-15 15:22:01 -0400

    create the structure of Shared Layout

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -1,5 +1,7 @@
 import { useAppContext } from '../context/appContext';
 import { Navigate } from 'react-router-dom';
+
+//this component protects the routes in dashboard
 const ProtectedRoute = ({ children }) => {
   const { user } = useAppContext();
 
