4cce0fb548c765d6820280b0a0714d5f11ef2419
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-16 23:25:47 -0400

    logout user

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -27,7 +27,7 @@ const initialState = {
   alertText: '',
   alertType: '',
   user: user ? JSON.parse(user) : null,
-  token: null,
+  token: token,
   userLocation: userLocation || '',
   jobLocation: userLocation || '',
   showSidebar: false,
@@ -66,7 +66,7 @@ const AppProvider = ({ children }) => {
     (error) => {
       console.log(error.response);
       if (error.response.status === 401) {
-        console.log('AUTH ERROR');
+        logoutUser();
       }
       return Promise.reject(error);
     }
@@ -162,10 +162,12 @@ const AppProvider = ({ children }) => {
       });
       addUserToLocalStorage({ user, location, token });
     } catch (error) {
-      dispatch({
-        type: UPDATE_USER_ERROR,
-        payload: { msg: error.response.data.msg },
-      });
+      if (error.response.status !== 401) {
+        dispatch({
+          type: UPDATE_USER_ERROR,
+          payload: { msg: error.response.data.msg },
+        });
+      }
     }
     clearAlert();
   };
