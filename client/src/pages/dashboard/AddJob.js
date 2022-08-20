fcf17dcfee4a079b374c3412f045eebee28d3cf9
nhussaini <nasrullah_hussaini@yahoo.com>
2022-08-19 21:43:32 -0400

    set SetEditJob Functinality

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -18,16 +18,17 @@ const AddJob = () => {
     handleChange,
     clearValues,
     createJob,
+    editJob,
   } = useAppContext();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // if (!position || !company || !jobLocation) {
-    //   displayAlert();
-    //   return;
-    // }
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
     if (isEditing) {
-      //eventually editJob()
+      editJob();
       return;
     }
     createJob();
